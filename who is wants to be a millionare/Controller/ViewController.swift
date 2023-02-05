ee3872c76a2dc309b82770768f4c65890b961141
tacarlen <l.feodotova296@yandex.ru>
2023-02-05 20:34:45 +0300

    first commit

---
+++ who is wants to be a millionare/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  who is wants to be a millionare
+//
+//  Created by Лилия Феодотова on 05.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
