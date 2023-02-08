f6c8e2893b45231531708dcbe7138b6013c0c6db
AndrewFrolenkov <andrewissuess@yahoo.com>
2023-02-08 19:55:00 +0300

    Create prompt FiftyFifty

--- who is wants to be a millionare/Model/QuestionModel.swift
+++ who is wants to be a millionare/Model/QuestionModel.swift
@@ -11,12 +11,14 @@ struct Question {
     var question: String
     var answers: [String]
     var correctAnswer: String
+    var notCorrectAnswer: String
     var money: Int
     
-    init(question: String, answers: [String], correctAnswer: String, money: Int) {
+    init(question: String, answers: [String], correctAnswer: String, notCorrectAnswer: String, money: Int) {
         self.question = question
         self.answers = answers
         self.correctAnswer = correctAnswer
+        self.notCorrectAnswer = notCorrectAnswer
         self.money = money
     }
 }
